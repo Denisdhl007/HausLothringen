038135737d928e52e1aa02f18cb21acf8ce7f81f
Denisdhl007 <denis.henin@icloud.com>
2023-02-08 15:02:46 +0100

    Suite

--- public/script/main.js
+++ public/script/main.js
@@ -246,55 +246,27 @@ function classToggle() {
 
 
 
-
-
   // SECTION 8
 
 
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-
-
+  let list = document.querySelectorAll(".list li");
+  let box = document.querySelectorAll(".box");
+  
+  list.forEach((el)=>{
+      el.addEventListener("click" , (e)=>{
+          list.forEach((el1)=>{
+              el1.style.color = "#000";
+          })
+          e.target.style.color = "#d4a373"
+          box.forEach((el2)=>{
+              el2.style.display = "none";
+          })
+          document.querySelectorAll(e.target.dataset.color).forEach((el3)=>{
+              el3.style.display = "flex";
+          })
+      })
+  })
+  
 
 
 
@@ -437,7 +409,7 @@ for (var i = 0; i < btns.length; i++) {
     "Real Estate",
     "Energy",
     "Wellness & Fitness",
-    "travel",
+    "Travel",
     "Health",
     "Consumers",
     "Food & Beverage",
